a78931d07cee8664c489185f756d923902cda9bc
anna Pfeifer <anna.pfeifer78@gmail.com>
2024-05-18 13:11:22 +0200

    Backend

--- StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
@@ -1,11 +1,23 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Profiles;
+using StudentAdminPortal.API.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StudentAdminContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDb"));
+});
+builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
